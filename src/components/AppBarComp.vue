004016388fd0726b7b258e24087f4936ed7d203a
Enrique Marin <enrique_marin_h@hotmail.com>
2022-08-15 11:06:09 -0600

    Add Fonts, Lottie Animation, Header

--- src/components/AppBarComp.vue
+++ src/components/AppBarComp.vue
@@ -6,6 +6,13 @@
           <img src="@/assets/img/logo-full.svg" alt="logo">
         </router-link>
       </div>
+
+      <div class="app-bar__menu">
+        <a href="#home">Inicio</a>
+        <a href="#about">Acerca de</a>
+        <a href="#services">Servicios</a>
+        <a href="#contact">Contacto</a>
+      </div>
     </div>
   </div>
 </template>
@@ -28,11 +35,32 @@
 .app-bar {
   padding: 3rem 2rem 1rem;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   .app-bar__logo {
     transition: var(--transition-normal);
     &:hover {
       transform: translateY(0.2rem);
+    }
+  }
+
+  .app-bar__menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    transition: var(--transition-normal);
+    column-gap: 2rem;
+
+    a {
+      text-decoration: none;
+      font-size: 1.2rem;
+      font-weight: var(--font-medium);
+      color: var(--color-text-dark);
+      transition: var(--transition-normal);
+      &:hover {
+        transform: translateY(0.2rem);
+      }
     }
   }
 }
--- src/components/home_view/HeaderComp.vue
+++ src/components/home_view/HeaderComp.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="header-comp">
     <div class="container">
-      <div class="row">
-        <h1 class="ts-biggest">
-          Creamos el <span class="tc-primary">sitio web</span> que necesitas con
-          un toque de creatividad y éxito
-        </h1>
-        <p class="ts-h2 w-75">
-          Comenzamos desde las bases y necesidades de tu negocio, estudiando
-          tus clientes y usuarios para descifrar la experiencia de usuario ideal
-          y de esa forma construir tu sitio web perfecto.
-        </p>
+      <div class="row align-items-center">
+        <div class="col col-12 col-sm-7">
+          <h1 class="main-title mb-4">
+            El <span class="tc-primary">sitio web</span> que tu negocio
+            necesita
+          </h1>
+          <p class="ts-h2 w-75 mb-4">
+            En <b>codera</b> te ofrecemos el expertise y el equipo para desarrollar
+            el sitio web para tu negocio.
+          </p>
+
+          <a href="#contact" class="fill">Cotiza tu idea</a>
+        </div>
+
+        <div class="col col-12 col-sm-5 d-none d-sm-flex">
+          <lottie-player src="./lottie-person.json" background="transparent"  speed="1"  style="width: 100%;" loop autoplay></lottie-player>
+        </div>
       </div>
     </div>
   </div>
@@ -29,5 +36,50 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
+}
+
+.main-title {
+  font-size: 3.6rem;
+  font-family: var(--belfast-font);
+}
+
+.button-start-project {
+  box-sizing: border-box;
+  background-color: var(--color-secondary);
+  padding: 0.6rem 1rem;
+  text-decoration: none;
+  color: var(--color-text);
+}
+
+.fill {
+  font-size: 20px;
+  letter-spacing: 1px;
+  padding: 13px 50px 13px;
+  outline: 0;
+  border: 1px solid rgb(26, 26, 26);
+  cursor: pointer;
+  position: relative;
+  text-decoration: none;
+  color: var(--color-title);
+  font-weight: var(--font-medium);
+  font-family: var(--belfast-font);
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.fill::after {
+  content: "";
+  background-color: var(--color-secondary);
+  width: 100%;
+  z-index: -1;
+  position: absolute;
+  height: 100%;
+  top: 7px;
+  left: 7px;
+  transition: 0.2s;
+}
+
+.fill:hover::after {
+  top: 0px;
+  left: 0px;
 }
 </style>
